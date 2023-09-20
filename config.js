3698dd3c7116120a83d8e2f11e71c55f6348e301
henrygraphic12 <144835387+henrygraphic12@users.noreply.github.com>
2023-09-20 06:52:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,14 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['2348184341492', 'BINASURV', true],
   [''] 
 ] //Number of owners
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['2348184341492','2348184341492'] 
+global.prems = ['2348184341492', '2348184341492', '2348184341492']
+global.allowed = ['2348184341492', '2348184341492', '2348184341492']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
